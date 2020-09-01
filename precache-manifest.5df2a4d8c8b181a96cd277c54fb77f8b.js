c3ce6ef2b653c46c70352af2b5044ec232491c11
Shubham Kumar <shubham@Shubhams-MacBook-Air.local>
2020-08-31 22:01:44 -0700

    Updates

---
+++ precache-manifest.5df2a4d8c8b181a96cd277c54fb77f8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5711cb24c888fef3898a984f6ca9850",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7ac8acb40ffb8cb6026",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "71d78c734eebea6ea713",
+    "url": "/static/css/main.b39b5750.chunk.css"
+  },
+  {
+    "revision": "c7ac8acb40ffb8cb6026",
+    "url": "/static/js/2.0789156f.chunk.js"
+  },
+  {
+    "revision": "b25e0c44d0b24745f7d62e2ea6be31e5",
+    "url": "/static/js/2.0789156f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71d78c734eebea6ea713",
+    "url": "/static/js/main.1e01945b.chunk.js"
+  },
+  {
+    "revision": "efe25b4b8d1f7af9da4b",
+    "url": "/static/js/runtime-main.6b3488d1.js"
+  }
+]);
